10a7c28941b576800e5babceb335ad3fe6ec56b9
gsosam99 <sosag@Guillermo-Sosa.local>
2020-09-10 10:41:34 -0400

    Assignment Module 1

---
+++ angularjs/module1-solution/app.js
@@ -0,0 +1,35 @@
+(function () {
+    'use strict';
+  
+    angular.module('LunchCheck', []).controller('LunchCheckController', LunchCheckController);
+  
+    LunchCheckController.$inject = ['$scope'];
+  
+    function LunchCheckController($scope) {
+      $scope.lunchItems = '';
+      $scope.messageStyle = { 'color': 'red' };
+      $scope.textboxStyle = { 'border-color': 'black' };
+      $scope.checkLunch = function () {
+        var numItems = $scope.lunchItems
+          .split(',')
+          .filter(x => x.trim() != '')
+          .length;
+  
+        if (numItems == 0) {
+          $scope.message = 'Please enter data first';
+          $scope.messageStyle = { 'color': 'red' };
+          $scope.textboxStyle = { 'border-color': 'red' };
+        } else if (numItems <= 3) {
+          $scope.message = 'Enjoy!';
+          $scope.messageStyle = { 'color': 'green' };
+          $scope.textboxStyle = { 'border-color': 'green' };
+        } else {
+          $scope.message = 'Too much!';
+          $scope.messageStyle = { 'color': 'green' };
+          $scope.textboxStyle = { 'border-color': 'green' };
+        }
+      }
+    }
+  
+  
+  })();
